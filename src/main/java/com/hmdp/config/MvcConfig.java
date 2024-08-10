f235c2603b2acefbb10f827e71afe70e2929b663
Jo <75053983+Nico-human@users.noreply.github.com>
2024-08-10 11:11:26 +0800

    Merge pull request #2
    * 0.2 测试启动
    
    * Feature 0.3 登录

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,38 @@
+package com.hmdp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description: MVC配置类
+ * @Author: dong
+ * @Date: 2024/8/9
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public MvcConfig(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+        // token刷新的拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+    }
+
+}
--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -3,9 +3,11 @@ package com.hmdp.controller;
 
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,9 +37,9 @@ public class UserController {
      * 发送手机验证码
      */
     @PostMapping("code")
-    public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        return Result.fail("功能未完成");
+    public Result<String> sendCode(@RequestParam("phone") String phone, HttpSession session) {
+        // 发送短信验证码并保存验证码
+        return userService.sendCode(phone, session);
     }
 
     /**
@@ -45,9 +47,8 @@ public class UserController {
      * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
      */
     @PostMapping("/login")
-    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        // TODO 实现登录功能
-        return Result.fail("功能未完成");
+    public Result<String> login(@RequestBody LoginFormDTO loginForm, HttpSession session){
+        return userService.login(loginForm, session);
     }
 
     /**
@@ -61,9 +62,10 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public Result me(){
-        // TODO 获取当前登录的用户并返回
-        return Result.fail("功能未完成");
+    public Result<UserDTO> me(){
+        // 获取当前登录的用户并返回
+        UserDTO user = UserHolder.getUser();
+        return Result.ok(user);
     }
 
     @GetMapping("/info/{id}")
