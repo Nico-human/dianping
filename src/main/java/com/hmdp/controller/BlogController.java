eb250942fdb1919e8a6cbb324845b9bb4fd6a7d7
Jo <75053983+Nico-human@users.noreply.github.com>
2024-08-13 18:22:55 +0800

    Merge pull request #5
    * Feature 1.1 发布点评与点赞
    
    * Feature 1.2 关注功能
    
    * Feature 1.2 关注功能

--- src/main/java/com/hmdp/controller/BlogController.java
+++ src/main/java/com/hmdp/controller/BlogController.java
@@ -1,15 +1,11 @@
 package com.hmdp.controller;
 
-
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
-import com.hmdp.entity.User;
 import com.hmdp.service.IBlogService;
-import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
-import com.hmdp.utils.UserHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -29,55 +25,54 @@ public class BlogController {
 
     @Resource
     private IBlogService blogService;
-    @Resource
-    private IUserService userService;
 
     @PostMapping
-    public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+    public Result<Long> saveBlog(@RequestBody Blog blog) {
+        Long blogId = blogService.saveBlog(blog);
+        return Result.ok(blogId);
     }
 
     @PutMapping("/like/{id}")
-    public Result likeBlog(@PathVariable("id") Long id) {
+    public Result<Boolean> likeBlog(@PathVariable("id") Long id) {
         // 修改点赞数量
-        blogService.update()
-                .setSql("liked = liked + 1").eq("id", id).update();
-        return Result.ok();
+        boolean liked = blogService.likeBlog(id);
+        return Result.ok(liked);
     }
 
     @GetMapping("/of/me")
-    public Result queryMyBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        // 根据用户查询
-        Page<Blog> page = blogService.query()
-                .eq("user_id", user.getId()).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
-        // 获取当前页数据
-        List<Blog> records = page.getRecords();
+    public Result<List<Blog>> queryMyBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
+        List<Blog> records = blogService.queryMyBlog(current);
         return Result.ok(records);
     }
 
     @GetMapping("/hot")
-    public Result queryHotBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
-        // 根据用户查询
-        Page<Blog> page = blogService.query()
-                .orderByDesc("liked")
-                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
-        // 获取当前页数据
-        List<Blog> records = page.getRecords();
-        // 查询用户
-        records.forEach(blog ->{
-            Long userId = blog.getUserId();
-            User user = userService.getById(userId);
-            blog.setName(user.getNickName());
-            blog.setIcon(user.getIcon());
-        });
+    public Result<List<Blog>> queryHotBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
+        List<Blog> records = blogService.queryHotBlog(current);
         return Result.ok(records);
     }
+
+    @GetMapping("/{id}")
+    public Result<Blog> queryBlogById(@PathVariable Long id) {
+        Blog blog = blogService.queryBlogById(id);
+        return blog != null ? Result.ok(blog) : Result.fail("笔记不存在");
+    }
+
+    @GetMapping("/likes/{id}")
+    public Result<List<UserDTO>> queryBlogLikes(@PathVariable Long id) {
+        return Result.ok(blogService.queryBlogLikes(id));
+    }
+
+    // BlogController  根据id查询博主的探店笔记
+    @GetMapping("/of/user")
+    public Result<List<Blog>> queryBlogByUserId(
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam("id") Long id) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
+
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +28,64 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    private final StringRedisTemplate stringRedisTemplate;
+    private final IUserService userService;
+
+    public FollowServiceImpl(StringRedisTemplate stringRedisTemplate, IUserService userService) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.userService = userService;
+    }
+
+    @Override
+    public Boolean follow(Long followUserId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        if (isFollow) {
+            // 关注
+            Follow follow = new Follow();
+            follow.setUserId(userId); // 自己的userId
+            follow.setFollowUserId(followUserId); // 关注的人的userId
+            boolean saved = this.save(follow);
+            if (saved) {
+                stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+            }
+        } else {
+            // 取关
+            boolean removed = this.remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId).eq("follow_user_id", followUserId));
+            if (removed) {
+                stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Boolean isFollow(Long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = this.query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        return count > 0;
+    }
+
+    @Override
+    public List<UserDTO> followCommons(Long id) {
+        // 1.获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        // 2.求交集
+        String key2 = "follows:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(key, key2);
+        if (intersect == null || intersect.isEmpty()) {
+            // 无交集
+            return Collections.emptyList();
+        }
+        // 3.解析id集合
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        // 4.查询用户
+        List<UserDTO> users = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return users;
+    }
 }
